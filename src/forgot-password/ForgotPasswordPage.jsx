12f066b8a435718b17c1393bb7f0a50ac8999462
waleed.mujahid <waleed.mujahid@arbisoft.com>
2026-01-01 18:32:39 +0500

    fix: update import statement for MultiSiteBannerContent
    Changed the import statement for MultiSiteBannerContent to use named import syntax for better compatibility with the module structure.

--- src/forgot-password/ForgotPasswordPage.jsx
+++ src/forgot-password/ForgotPasswordPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 // Todo: need to change imports when package is published to edly-io
-import MultiSiteBannerContent from '@anas_hameed/edly-saas-widget';
+import { MultiSiteBannerContent } from '@anas_hameed/edly-saas-widget';
 import { getConfig } from '@edx/frontend-platform';
 import { sendPageEvent, sendTrackEvent } from '@edx/frontend-platform/analytics';
 import { useIntl } from '@edx/frontend-platform/i18n';
