7db4b0264bbfdaa2c5f7976c6c5228652e2e99e8
Anas-hameed <anas.hameed@arbisoft.com>
2026-01-23 15:11:12 +0500

    Fix: resolve duplicate issue

--- src/login/LoginPage.jsx
+++ src/login/LoginPage.jsx
@@ -1,7 +1,7 @@
 import {
   useCallback, useEffect, useMemo, useState,
 } from 'react';
-import { useDispatch, useSelector, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Todo: need to change imports when package is published to edly-io
 import { EdlyLogistrationInfo } from '@anas_hameed/edly-saas-widget';
