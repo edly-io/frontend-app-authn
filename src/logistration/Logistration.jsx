9029024a8d2396d08aa181c92f8f02f4e4021d00
Anas-hameed <anas.hameed@arbisoft.com>
2026-02-03 19:57:19 +0500

    Fix: resolve the issue with connect import

--- src/logistration/Logistration.jsx
+++ src/logistration/Logistration.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector, connect } from 'react-redux';
 
 // Todo: need to change imports when package is published to edly-io
 import { emailCheckComplete, EmailCheckWidget } from '@anas_hameed/edly-saas-widget';
